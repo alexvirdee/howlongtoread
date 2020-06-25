440de61944c3b9c8976b9b2f7abf11fb84817fc7
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-25 14:23:18 -0400

    Navigation margins + color change

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,29 +18,29 @@ class Header extends React.Component {
     render() {
         return (
             <div className="header">
-                <nav className="flex items-center justify-between flex-wrap bg-blue-400 p-6 w-full z-10 top-0">
-                    <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <nav className="flex items-center justify-between flex-wrap bg-blue-800 p-6 w-full z-10 top-0 sm:px-6 py-1 px-2">
+                    <div className="flex items-center">
                         <Link className="text-white no-underline hover:text-white hover:no-underline" to="/">
                             <span className="text-2xl pl-2"><img className='logo' src={Logo} alt='logo' /> </span>
                         </Link>
                     </div>
             
                     <div className="block lg:hidden">
-                        <button onClick={this.toggleNav} id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-blue-800 hover:border-blue-800">
+                        <button onClick={this.toggleNav} id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-blue-400 hover:border-blue-400">
                             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                         </button>
                     </div>
             
-                    <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block pt-6 lg:pt-0" id="nav-content">
+                    <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-8" id="nav-content">
                         <ul className="list-reset lg:flex justify-end flex-1 items-center">
                             <li className="mr-3">
-                                <Link className="inline-block py-2 px-4 text-white hover:text-blue-800 no-underline" to="/">Home</Link>
+                                <Link className="inline-block py-2 px-4 text-white hover:text-blue-400 no-underline" to="/">Home</Link>
                             </li>
                             <li className="mr-3">
-                                <Link className="inline-block text-white no-underline hover:text-blue-800 hover:text-underline py-2 px-4" to="/about">About</Link>
+                                <Link className="inline-block text-white no-underline hover:text-blue-400 hover:text-underline py-2 px-4" to="/about">About</Link>
                             </li>
                             <li className="mr-3">
-                                <a className="inline-block text-white no-underline hover:text-blue-800 hover:text-underline py-2 px-4" href="http://www.alexvirdee.com/">Portfolio</a>
+                                <a className="inline-block text-white no-underline hover:text-blue-400 hover:text-underline py-2 px-4" href="http://www.alexvirdee.com/">Portfolio</a>
                             </li>
                         </ul>
                     </div>
--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -12,10 +12,10 @@ const SearchBar = () => {
         className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
         id="search"
         type="text"
-        placeholder="Search"
+        placeholder="Try 'The Hunt For Red October by Tom Clancy' "
      />
         <div className="p-4">
-          <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+          <button className="bg-blue-800 text-white rounded-full p-2 hover:bg-blue-600 focus:outline-none w-12 h-12 flex items-center justify-center">
             <FontAwesomeIcon icon={faSearch} />
           </button>
         </div>
