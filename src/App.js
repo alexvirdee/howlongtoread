f6ec6f2a4256799379ce6bbd97b5343182850407
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-25 12:12:30 -0400

    Organize component folder structure & add tailwinds css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+import HomePage from './pages/homepage/homepage.component';
+
+import Header from './components/header/header.component';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <Header />
+        <BrowserRouter>
+           <Switch>
+            <Route exact path='/' component={HomePage} />
+           </Switch>
+           </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='hoempage'>
+        <h1 className='text-4xl'>Homepage</h1>
+    </div>
+)
+
+export default HomePage;
